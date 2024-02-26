0dcb2b1ab3056f243e35bf16ec69c1002468060d
Matheus Gallo <matheusgallo3@gmail.com>
2024-02-26 17:54:42 -0300

    feat: add crud routes

---
+++ router/routes.go
@@ -0,0 +1,55 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine) {
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", func(ctx *gin.Context) {
+
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Get Opening",
+			})
+		})
+	}
+
+	{
+		v1.POST("/opening", func(ctx *gin.Context) {
+
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Post Opening",
+			})
+		})
+	}
+
+	{
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Delete Opening",
+			})
+		})
+	}
+
+	{
+		v1.PUT("/opening", func(ctx *gin.Context) {
+
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Update Opening",
+			})
+		})
+	}
+
+	{
+		v1.GET("/openings", func(ctx *gin.Context) {
+
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "Get Openings",
+			})
+		})
+	}
+}
