7f3c69cc0b0f3606d32cbd3eaf5969a4438f4392
Matheus Gallo <matheusgallo3@gmail.com>
2024-02-28 13:33:33 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"github.com/matheusgallo/gopportunities/config"
 	"github.com/matheusgallo/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
 )
 
 func main() {
 
+	logger = config.GetLogger("main")
+	//init configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//init router
 	router.Initialize()
 
 }
